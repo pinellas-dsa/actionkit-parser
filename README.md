3212d3c0e177400bb0586f9f3c2292038959863b
Jamie <jamiek5@users.noreply.github.com>
2022-04-10 19:39:37 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Command-line tool to parse ActionKit reports and format them as a Spoke campaign
+
+Usage: ./actionkit-parser [name_of_input_file.csv] [optional_name_of_output_file.csv]
+
+Currently does not support non-US/CA numbers.
---
+++ actionkit-parser.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+import csv
+
+try:
+    fname = sys.argv[1]
+except IndexError:
+    print('Run as ./actionkit-parser [name_of_input_file.csv] [optional_name_of_output_file.csv]', file=sys.stderr)
+    sys.exit(1)
+
+try:
+    filename = sys.argv[2]
+except IndexError:
+    filename = 'output.csv'
+
+
+# todo: if need be, add something to catch international numbers or malformed numbers
+def parse_phone(num):
+    if len(num) == 10:
+        return num
+    elif len(num) > 10:
+        return num[:10]
+
+
+header = ['firstName', 'lastName', 'cell']
+with open(fname, 'r') as infile, open(filename, 'w', newline='') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+    next(reader)  # skip the header
+    writer.writerow(header)  # add our header
+    for row in reader:
+        if row[21] == 'TRUE':  # skip anyone who is p2ptext_optout
+            continue
+        first = row[1]
+        last = row[3]
+        if row[15]:  # if they filled in the mobile number field, use that
+            cell = row[15]
+        else:
+            cell = row[16]  # otherwise, use the generic "phone" field
+        cell = parse_phone(cell)
+        if cell:
+            writer.writerow([first, last, cell])
+
+
